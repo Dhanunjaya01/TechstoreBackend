2a24e5bb8a76ee41770f3a13be958abaea4abece
Dhanush <dhanushdhanu90143@gmail.com>
2026-07-05 15:24:47 +0530

    Update backend URL

--- src/main/java/com/techstore/controller/GetAllProductsServlet.java
+++ src/main/java/com/techstore/controller/GetAllProductsServlet.java
@@ -77,7 +77,7 @@ public class GetAllProductsServlet extends HttpServlet {
             HttpServletResponse response)
             throws ServletException, IOException {
 
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+        response.setHeader("Access-Control-Allow-Origin", "https://techstore-store.vercel.app/");
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type");
         response.setStatus(HttpServletResponse.SC_OK);
